2003f4462a1c042eb1e94c0309e8bdfd6e55319f
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-06-16 17:35:39 +0530

    Removed tests

--- BlockOptimiser/test/test_utility.py
+++
@@ -1,6 +0,0 @@
-def follows_topo_order(order, adj_list):
-    for u in adj_list:
-        for v in u.parents:
-            if v in order and order.index(v) < order.index(u):
-                return False
-    return True
